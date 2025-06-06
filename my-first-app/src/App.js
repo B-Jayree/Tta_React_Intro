a8a3ccb82c33dfa3448c925861f4d4fca42de2b0
B-Jayree <benybwitonzi@gmail.com>
2025-06-06 22:14:46 +0200

    End of  Week 9

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -4,12 +4,41 @@ import './App.css';
 // import ClassComp from "./components/ClassComp";
 import Heading from "./components/Heading";
 import Teams from "./components/Teams";
+import maleMember  from "./assets/male.jfif";
+import femaleMember  from "./assets/female.jfif";
+
 
 function App() {
+  const memberList = [
+    {
+      image: femaleMember,
+      name : "Annette Black",
+      title: "Dog Trainer",
+      symbol: "♠"
+    },
+    {
+      image: maleMember,
+      name : "Wade Warren",
+      title: "Medical Assistant",
+      symbol: "♣"
+    },
+    {
+      image: femaleMember,
+      name : "Savannah Nguye",
+      title: "President of sales",
+      symbol: "♥"
+    },
+    {
+      image: maleMember,
+      name : "Jenny Wilson",
+      title: "Nursing Assistant",
+      symbol: "♦"
+    }
+  ]
   return (
     <div className="App">
       <Heading/>
-      <Teams/>
+      <Teams members={memberList}/>
          
         {/* <Functional/>
         <ClassComp/> */}
--- my-first-app/src/components/Teams.jsx
+++ my-first-app/src/components/Teams.jsx
@@ -1,34 +1,20 @@
 import React from 'react'
-import maleMember  from "../assets/male.jfif";
-import femaleMember  from "../assets/female.jfif";
 
-export default function Teams() {
+
+export default function Teams({ members}) {
+    console.log(members)
   return (
     <div className="main-container">
-        <div className="member-container">
-            <img src={maleMember} alt="Male worker" height="200" width="200" />
-                <p className="member-title">Medical Assistant</p>
-                <h3>Wade Warren</h3>
-                <p>&clubs;</p>
-        </div>
-        <div class="member-container">
-                <img src={maleMember} alt="Male worker" height="200" width="200" />
-                <p class="member-title">Nursing Assistant</p>
-                <h3>Jenny Wilson</h3>
-                <p>&diams;</p>
-            </div>
-             <div class="member-container">
-                <img src={femaleMember} alt="Female worker" height="200" width="200" />
-                <p class="member-title">Dog Trainer</p>
-                <h3>Annette Black</h3>
-                <p>&spades;</p>
-            </div>
-             <div class="member-container">
-                 <img src={femaleMember} alt="Female worker" height="200" width="200" />
-                <p class="member-title">President of sales</p>
-                <h3>Savannah Nguyen</h3>
-                <p>&hearts;</p>
-            </div>  
+        {members.map((member, index) => (
+            <div className="member-container" key={index}>
+                <img src={member.image} alt={member.name} height="200" width="200" />
+                 <p className="member-title">{member.title}</p>
+                <h3>{member.name}</h3>
+                <p>{member.symbol}</p>
+                </div>
+
+        ) )}
+        
     </div>
   )
 }
