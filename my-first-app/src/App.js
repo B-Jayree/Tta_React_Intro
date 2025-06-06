ac8d1f05a4fbbf98cfb3a37a1dc0f677a164cda1
B-Jayree <benybwitonzi@gmail.com>
2025-06-06 19:43:56 +0200

    Week 9 Day 2

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Hello World!</p>
+        <div className="main-heading">
+            <div>
+                <h1>Meet The Team</h1>
+                <p>Meet our team of professionals to serve you</p>
+            </div>
+            <div>
+                <button class="borderless-buttons about-button " title="go to about us page">About us</button>
+                <button class=" borderless-buttons contact-button" title="Go to contact us page">Contacts</button>
+            </div>
+
+        </div>
      
     </div>
   );
